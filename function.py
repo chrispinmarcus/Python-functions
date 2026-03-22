9f9ff4bf99b5134784c57c5fc3b4207cbaf7dfd8
ArseneMerci <harsenemerci@gmail.com>
2026-03-22 15:45:39 +0200

    python functions

---
+++ function.py
@@ -0,0 +1,8 @@
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"you are {age} years old!")
+    print("Happy birthday to you Men!")
+    print()
+happy_birthday("Chris", 30)
+happy_birthday("Linda", 40)
+happy_birthday("Leila", 50)
